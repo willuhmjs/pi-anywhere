0c24c4baeb97632e4825bfc06a364c7053891175
Will Faircloth <willfaircloth@icloud.com>
2021-06-16 16:05:40 -0400

    Basic test code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const ngrok = require("ngrok");
+const { token }= require("./config");
+
+(async () => {
+  await ngrok.authtoken(token);
+  const url = await ngrok.connect({ proto: "tcp", addr: 22});
+  console.log(url)
+})();
