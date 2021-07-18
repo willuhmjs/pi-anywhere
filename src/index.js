81aa84b4b8fb144b2f968b65310a18bb537a253a
Will Faircloth <willfaircloth@icloud.com>
2021-07-18 12:06:07 -0400

    Add webhook functionality/feature

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+// Critical thinking owo
 const ngrok = require("ngrok");
-const { token } = require("./config");
+const { token, webhookId, webhookToken } = require("./config");
 
+// Get discord.js and connect the Discord webhook
+const Discord = require("discord.js");
+const webhook = new Discord.WebhookClient(webhookId, webhookToken);
 
+// Create the thing and send it!
 (async () => {
+  // Connect to ngrok
   await ngrok.authtoken(token);
   const url = await ngrok.connect({ proto: "tcp", addr: 22});
-  console.log(url)
+
+  // Create the embed to send to the webhook
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Server Created!")
+    .setDescription(url)
+    .setFooter("A server has been created for your Pi.")
+    .setTimestamp()
+    .setColor("GREEN");
+
+    // Send embed to webhook as well as the link for easy mobile copying
+    webhook.send(url, {embeds: [embed]});
+    console.log(`Server Created; ${url}`);
 })();
