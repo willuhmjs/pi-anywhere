fabd1a8f7dc5c7966ecb1f2de91861ea44a00a10
Will Faircloth <willfaircloth@icloud.com>
2021-09-12 11:38:25 -0400

    Change from webhook id, token to bot token.

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   token: process.env.TOKEN,
-  webhookId: process.env.WEBHOOKID,
-  webhookToken: process.env.WEBHOOKTOKEN
+  botToken: process.env.BOTTOKEN;
 }
